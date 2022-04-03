8357cb1c40d24d76ecb6f11374504a8d036babdc
Rowan S-L <rowan.with.cats@gmail.com>
2022-04-03 07:18:20 -0400

    reorganized slightly

--- code/bot/src/main.rs
+++ code/bot/src/main.rs
@@ -13,39 +13,11 @@ use aareocams_scomm::Stream;
 use anyhow::Result;
 use tokio::net::TcpListener;
 
+
 mod config {
     pub const ADDR: &str = "127.0.0.1:6440";
 }
 
-pub trait Encoder {
-    /// gets the range of output values
-    fn range(&self) -> (usize, usize);
-
-    /// updates the encoder, receiveing values
-    /// and storing them for use in the
-    /// `range` `rotation` and `net_change` functions
-    fn update(&mut self);
-
-    /// gets the current encoder rotation
-    fn rotation(&self) -> usize;
-
-    /// gets the net change in location since it was last checked
-    fn net_change(&self) -> usize;
-}
-
-/// a simple motor controller implementation.
-///
-/// minimum speed is 0, maximum speed is 100, and can be made negative to reverse the motor
-pub trait MotorController {
-    /// inverse the motor direction
-    fn inverse(&mut self);
-
-    /// sets the number of seconds it takes to go to maximmum speed (open loop ramp rate)
-    fn set_ol_ramp_rate(&mut self, rate: f32);
-
-    /// sets the motors current speed (-100 to 100) (reversed full speed to full speed)
-    fn set_speed(&mut self, speed: f32);
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -79,4 +51,36 @@ async fn main() -> Result<()> {
             );
         }
     }
+}
+
+/// interface to the hardware of a quadrature encoder (with index)
+pub trait QIEncoderInterface {
+    /// get the encoder resolution in PPR
+    fn get_res(&mut self) -> usize;
+
+    // rotational progression:
+    // NOTE: i could be 1 at any point throughout, but it can only be 1 at ONE PLACE and that place stays constant
+    //
+    // a b i
+    // 0 0 1
+    // 1 0 0
+    // 1 1 0
+    // 0 1 0
+    //
+    /// reads the three channels of the encoder (a, b, index)
+    fn get_raw_vals(&self) -> [bool; 3];
+}
+
+/// a simple motor controller implementation.
+///
+/// minimum speed is 0, maximum speed is 100, and can be made negative to reverse the motor
+pub trait MotorController {
+    /// inverse the motor direction
+    fn inverse(&mut self);
+
+    /// sets the number of seconds it takes to go to maximmum speed (open loop ramp rate)
+    fn set_ol_ramp_rate(&mut self, rate: f32);
+
+    /// sets the motors current speed (-100 to 100) (reversed full speed to full speed)
+    fn set_speed(&mut self, speed: f32);
 }
